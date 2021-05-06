6f402b6707636a9f8f7e3b245d11b50b1f0fe501
Pia-Maria Michnik <pia.michnik@gmail.com>
2021-05-06 20:30:51 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var map = L.map('map').setView([51.4389629,7.3253178], 12);
+var map = L.map('map').setView([51.4389629,7.3253178], 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-/*L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();*/
+
+fetch('data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    
+    for (let i = 0; i < data.events; i++) {
+        let event = data.events[i];
+
+        L.marker([event.latitude, event.longitude]).addTo(map)
+        .bindPopup(event.id);
+    }
+  });
+
+
